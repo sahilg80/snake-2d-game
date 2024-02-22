14c5d4fb20bc8d1b72482ce47b4797826c1f1e7f
sahilg80 <38989332+sahilg80@users.noreply.github.com>
2024-02-22 15:05:00 +0530

    Added grid background and snake

--- Assets/Scripts/GameAssets.cs
+++ Assets/Scripts/GameAssets.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class GameAssets : MonoBehaviour
 {
+    public static GameAssets Instance { get; set; }
+
+    public Sprite snakeBodySprite;
+    public Sprite snakeHeadSprite;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,6 +7,7 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Sprite sprite = GameAssets.Instance.snakeBodySprite;
         
     }
 
